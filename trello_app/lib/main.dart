f794e0ba5a0fc716a7b39a379e75261212be201b
Varun Kumar Madupu <varunkumarmadupu@Varuns-MacBook-Air.local>
2019-07-15 15:44:27 +0530

    restructure

---
+++ trello_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:trello_app/home_screen.dart';
+
+void main() => runApp(TrelloCloneApp());
+
+class TrelloCloneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trello Clone',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
